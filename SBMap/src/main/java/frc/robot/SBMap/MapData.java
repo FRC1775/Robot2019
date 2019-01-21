3bde36ad155eee28a9a4fe9a1e56aee822c86216
Tigerbytes 1775 Team Account <tigerbytes1775@gmail.com>
2019-01-21 13:25:51 -0600

    Start of Shuffleboard Map

---
+++ SBMap/src/main/java/frc/robot/SBMap/MapData.java
@@ -0,0 +1,45 @@
+package frc.robot.SBMap;
+
+import edu.wpi.first.shuffleboard.api.data.ComplexDataType;
+import edu.wpi.first.shuffleboard.api.util.Maps;
+
+import frc.robot.SBMap.Point2D;
+
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * Represents data of the {@link Point2D} type.
+ */
+public final class MapData extends ComplexDataType<Point2D> {
+
+  /**
+   * The name of data of this type as it would appear in a WPILib sendable's {@code .type} entry; a differential drive
+   * base a {@code .type} of "DifferentialDrive", a sendable chooser has it set to "String Chooser"; a hypothetical
+   * 2D point would have it set to "Point2D".
+   */
+  private static final String TYPE_NAME = "Point2D";
+
+  /**
+   * The single instance of the point type. By convention, this is a {@code public static final}
+   * field and the constructor is private to ensure only a single instance of the data type exists.
+   */
+  public static final MapData Instance = new MapData();
+
+  private MapData() {
+    super(TYPE_NAME, Point2D.class);
+  }
+
+  @Override
+  public Function<Map<String, Object>, Point2D> fromMap() {
+    return map -> new Point2D(
+        (double)Maps.getOrDefault(map, "x", (Object)0),
+        (double)Maps.getOrDefault(map, "y", 0)
+    );
+  }
+
+  @Override
+  public Point2D getDefaultValue() {
+    return new Point2D(0, 0);
+  }
+}
---
+++ SBMap/src/main/java/frc/robot/SBMap/MapWidget.java
@@ -0,0 +1,41 @@
+package frc.robot.SBMap;
+
+import edu.wpi.first.shuffleboard.api.widget.Description;
+import edu.wpi.first.shuffleboard.api.widget.ParametrizedController;
+import edu.wpi.first.shuffleboard.api.widget.SimpleAnnotatedWidget;
+
+import frc.robot.SBMap.Point2D;;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+@Description(
+    name = "Simple Point",
+    dataTypes = MapData.class,
+    summary = "Displays the X and Y coordinates of a point"
+)
+@ParametrizedController("SimplePointWidget.fxml")
+public final class MapWidget extends SimpleAnnotatedWidget<Point2D> {
+
+  @FXML
+  private Pane root;
+  @FXML
+  private Label xCoordinateView;
+  @FXML
+  private Label yCoordinateView;
+
+  @FXML
+  private void initialize() {
+    // Bind the text in the labels to the data
+    // If you are unfamiliar with the -> notation used here, read the Oracle tutorial on lambda expressions:
+    // https://docs.oracle.com/javase/tutorial/java/javaOO/lambdaexpressions.html
+    xCoordinateView.textProperty().bind(dataOrDefault.map(point -> point.getX()).map(x -> "X: " + x));
+    yCoordinateView.textProperty().bind(dataOrDefault.map(point -> point.getY()).map(y -> "Y: " + y));
+  }
+
+  @Override
+  public Pane getView() {
+    return root;
+  }
+}
