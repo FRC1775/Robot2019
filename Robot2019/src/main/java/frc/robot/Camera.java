aceb9277ea49d9ea0b4da5405e4ebc0b0b3b4568
tigerbytes1775 <tigerbytes1775@gmail.com>
2019-02-16 22:34:20 -0600

    troubleshooting interpreting the angle

--- Robot2019/src/main/java/frc/robot/Camera.java
+++ Robot2019/src/main/java/frc/robot/Camera.java
@@ -1,10 +1,8 @@
 package frc.robot;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 import org.opencv.core.Mat;
 import org.opencv.core.Rect;
@@ -48,6 +46,8 @@ public class Camera  {
 
   public void init(){
     new Thread(() -> {
+
+      // set up the camera and the smart dashboard videos
       UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
       camera.setResolution(320, 240);
       camera.setExposureManual(0);
@@ -64,12 +64,12 @@ public class Camera  {
         if(frameTime == 0){
           continue; 
         }
-        
         pipeline.process(source);
 
         // Drawing contours on the SmartDashboard
         output = pipeline.maskOutput(); 
         ArrayList<MatOfPoint> contours = pipeline.goodBoiArray();
+        SmartDashboard.putNumber("contours size", contours.size());
         // Always draw contours
         if (contours.size() > 0) {
           Imgproc.drawContours(output, contours, -1, new Scalar(0, 255, 0));
@@ -86,10 +86,13 @@ public class Camera  {
           }
         }
         outputStream.putFrame(output);
-        ArrayList<MatOfPoint> greaterBoiArray = pipeline.goodBoiArray();
-        if (greaterBoiArray.size() > 0) {
-          compareAngles(greaterBoiArray);
-          Rect r = Imgproc.boundingRect(greaterBoiArray.get(0));
+
+        if (pipeline.goodBoiArray().size() > 0) {
+          // find the index in goodBoiArray of the rightmost strip of tape 
+          // for our target hatch. This is the tape we will use to preform distance calculations.
+          int indexOfRightTarget = compareAngles(pipeline.goodBoiArray());
+          SmartDashboard.putNumber("index of right target", indexOfRightTarget);
+          Rect r = Imgproc.boundingRect(pipeline.goodBoiArray().get(indexOfRightTarget));
           synchronized (imgLock) {
             perimeter = 2 * r.width + 2 * r.height;
             area = r.width * r.height;
@@ -113,7 +116,7 @@ public class Camera  {
           */
           //System.out.println( "distance height: " + distanceHeight );
           SmartDashboard.putNumber("DistanceHeight:", distanceHeight);
-          SmartDashboard.putBoolean("GoodBoiArray has values in it", greaterBoiArray.size() > 0);
+          SmartDashboard.putBoolean("GoodBoiArray has values in it", pipeline.goodBoiArray().size() > 0);
           //System.out.println ("distance width: " + distanceWidth );
           //System.out.println("width: " + r.width);
           //System.out.println("height: " + r.height);
@@ -122,37 +125,73 @@ public class Camera  {
     }).start();      
   }
 
-
+  // class created to allow us to store the x value of the center of
+  // a rotated rectangle, the angle at which that rectangle is rotated,
+  // and its index in goodBoiArray.
   public class XAndAngle{
-    public double XVal;
-    public double AngleVal;
+    public double xVal;
+    public double angleVal;
+    public int originalIndex;
 
-    public XAndAngle (double XVal, double AngleVal){
-      this.XVal = XVal;
-      this.AngleVal = AngleVal;
+    public XAndAngle (double xVal, double angleVal, int originalIndex){
+      this.xVal = xVal;
+      this.angleVal = angleVal;
+      this.originalIndex = originalIndex;
     }
   }
 
-  public void compareAngles(ArrayList<MatOfPoint> contours){
-    MatOfPoint angleOne;
+  
+  public int compareAngles(ArrayList<MatOfPoint> contours){
+    // Finds the rotated rectangle for each of the strips of tape we see, 
+    // then stores them in angles_array as (center x value, angle of rotation, index in goodBoiArray)
+    ArrayList<XAndAngle> angles_array = findRotatedRectangle(contours);
+
+    // If there are at least two pieces of tape being seen, determine which is to the right of a target.
+    if(angles_array.size() >= 2){
+      return findTarget(angles_array);
+    }else{
+      // There is only one element in the array. We assume for now that it's the right target, though
+      // we need to write code that handles if it's the left target instead. 
+      return 0;
+    }
+  }
+
+  // for each of the contours, generates a rotated rectangle and puts its 
+  // center X coordinae and angle of rotation in an array
+  public ArrayList<XAndAngle> findRotatedRectangle(ArrayList<MatOfPoint> contours){
+    MatOfPoint originalContours;
     RotatedRect bounds;
     double boundingAngle;
     ArrayList<XAndAngle> angles_array = new ArrayList<>();
-    Mat points = new Mat();
+    double centerx;
+    int index;
+    // looping through goodBoiArray
     for(int i = 0; i < contours.size(); i++){
-      angleOne = contours.get(i);
-      MatOfPoint2f angleOneModified = new MatOfPoint2f(angleOne.toArray());
+      originalContours = contours.get(i);
+      MatOfPoint2f angleOneModified = new MatOfPoint2f(originalContours.toArray());
+      // get the rotated rectangle
       bounds = Imgproc.minAreaRect(angleOneModified);
+      // before, the angle is from -90 to 0; we want it from 0 to 180
       boundingAngle = adjustAngle(bounds);
-      double centerx = bounds.center.x;
-      int index = sortArray(angles_array, centerx);
-      angles_array.add(index, new XAndAngle(centerx, boundingAngle));
+      centerx = bounds.center.x;
+      // Orders the array from smallest x value to largest.
+      // We're ordering as we put values in the array as an attempt to not 
+      // have to iterate through the whole thing.
+      index = sortArray(angles_array, centerx);
+      angles_array.add(index, new XAndAngle(centerx, boundingAngle, i));
+      // this was for when we had two strips of tape in the field of view. 
+      // It can be modified to suit any number of strips.
       if(i == 0){
         SmartDashboard.putNumber("Angle 0", boundingAngle);
-      } else{
+      } else if(i == 1) {
         SmartDashboard.putNumber("Angle 1", boundingAngle);
+      }else if(i == 2){
+        SmartDashboard.putNumber("Angle 2", boundingAngle);
       }
     }
+
+    // returns a list of (centerX, rotated angle, index in goodBoiArray)
+    return angles_array;
   }
 
   // puts the angle of the rectangle on a scale of 0 - 180
@@ -164,15 +203,52 @@ public class Camera  {
     }
   } 
 
+  // Returns the index where the centerX value should be put in the array 
+  // to make the array ordered from least to greatest centerX values
   public int sortArray(ArrayList<XAndAngle> array, double centerX){
     double currentX;
+    // If the array is empty, center x is our smallest value. Put it first
+    if(array.isEmpty()){
+      return 0;
+    }
+
+    // Compare the x value to be sorted to those already in the array.
+    // Values in the array increase as we iterate through it. The first 
+    // value in the array to be greater than the one we want to place tells
+    // us where to place x. 
     for(int i = 0; i < array.size(); i++){
-      currentX = array.get(i).XVal;
+      currentX = array.get(i).xVal;
       if(centerX < currentX){
         return i;
       }
     }
+
+    // Center x is greater than every other value in the array. Put it last
     return array.size() - 1;
+  }
+
+  public int findTarget(ArrayList<XAndAngle> array){
+    double angleSum;
+    for(int i = 0; i < array.size() - 1; i++){
+      // Check that the angles are close to supplementary as an extra 
+      // filter to verify that we have the correct contours
+      angleSum = array.get(i).angleVal + array.get(i + 1).angleVal;
+      if(160 <= angleSum && angleSum <= 200
+        && array.get(i).angleVal < 90 && array.get(i + 1).angleVal > 90){
+        // If both are angled inwards, the target is in between them.
+        // We want to always return the rightmost strip of tape
+        return array.get(i + 1).originalIndex;
+      }
+    }
+
+    /* In the case where there are at least two strips of tape, but none are 
+     * facing each other, the tape must be positioned like this: \  /
+     * This means that the leftmost strip of tape is actually the rightmost
+     * piece of tape for a target, and the other piece is out of the field of view.
+     * To be consistent and always return the rightmost tape of a pair, we will
+     * return the first (index 0) element of the array of tape strips. 
+     */
+    return array.get(0).originalIndex;
   }
 }
 
