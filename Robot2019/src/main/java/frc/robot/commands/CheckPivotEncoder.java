fe14666b6c55f7a26ac056f2b1026d3f2d307abc
Tigerbytes 1775 Team Account <tigerbytes1775@gmail.com>
2019-03-16 14:47:47 -0500

    stupid

--- Robot2019/src/main/java/frc/robot/commands/CheckPivotEncoder.java
+++ Robot2019/src/main/java/frc/robot/commands/CheckPivotEncoder.java
@@ -18,11 +18,7 @@ import frc.robot.OI;
  * The left joystick controls our forward/backward movement, and the right joystick controls turning. 
  */
 public class CheckPivotEncoder extends Command {
-  public final static double ENCODER_RANGE = 4.972;
-  public final static double CONVERSION_FACTOR = 360 / ENCODER_RANGE; 
-  public static double init_volts = 0;
-  public static double init_angle = 0;
-  public static double angle = 0;
+
   public CheckPivotEncoder() {
     // Use requires() here to declare subsystem dependencies
     requires(Robot.pivotEncoderSubsystem);
@@ -33,26 +29,16 @@ public class CheckPivotEncoder extends Command {
   protected void initialize() {
     //RobotMap.pivotEncoder.initAccumulator();
     //RobotMap.pivotEncoder.resetAccumulator();
-    init_volts = RobotMap.pivotEncoder.getVoltage();
-    init_angle = init_volts * CONVERSION_FACTOR;
+
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
 
-    double volts = RobotMap.pivotEncoder.getVoltage() - init_volts;
-    if (volts < 0){
-      volts += ENCODER_RANGE;
-    }
-    angle = volts * CONVERSION_FACTOR;
-    SmartDashboard.putNumber("encoder voltage", volts);
-    SmartDashboard.putNumber("angle of pivot", angle);
-    SmartDashboard.putNumber("initial voltage", init_volts);
+
   }
-  public static double getAngle(){
-    return angle;
-  }
+
   @Override
   protected boolean isFinished() {
     return false;
