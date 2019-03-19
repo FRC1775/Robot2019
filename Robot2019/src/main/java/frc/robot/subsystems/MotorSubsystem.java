fc68e5e7bb6a9e858872af03b7d118459714a2fe
tigerbytes1775 <tigerbytes1775@gmail.com>
2019-03-19 15:17:15 -0500

    no pid thanks

--- Robot2019/src/main/java/frc/robot/subsystems/MotorSubsystem.java
+++ Robot2019/src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -19,8 +19,8 @@ import frc.robot.commands.Drive;
 /**
  * An example subsystem.  You can replace me with your own Subsystem.
  */
-  public class MotorSubsystem extends Subsystem implements PIDSource {
-  private PIDController rotateToAnglePidController; 
+  public class MotorSubsystem extends Subsystem {
+  
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
@@ -28,20 +28,7 @@ import frc.robot.commands.Drive;
   long startTime = 0;
   double rampFactor;  
  
-  public void MotorSubsystem(){
-   //.15, 0, .45 
-		rotateToAnglePidController = new PIDController(0, 0, 0, (PIDSource) RobotMap.gyro,
-    (value) ->  {
-      if (rotateToAnglePidController.isEnabled()) {
-        RobotMap.drive.arcadeDrive(0, value);
-      }
-    }, 0.02);
-
-  rotateToAnglePidController.setInputRange(-180, 180);
-  rotateToAnglePidController.setOutputRange(-0.75, 0.75);
-  rotateToAnglePidController.setAbsoluteTolerance(2);
-  rotateToAnglePidController.setContinuous();
-  }
+ 
 
   @Override
   public void initDefaultCommand() {
@@ -77,7 +64,7 @@ import frc.robot.commands.Drive;
       // System.out.println("we're trying to turn but not move");
 
       rampFactor = Math.min(1, (System.currentTimeMillis() - startTime) / RAMP_TIME);
-      turnValue = rampFactor * xVal;
+      turnValue = rampFactor * xVal * 0.8;
       moveValue = 0;
       SmartDashboard.putNumber("Current Time", System.currentTimeMillis());
       SmartDashboard.putNumber("Start Time", startTime);
@@ -87,53 +74,9 @@ import frc.robot.commands.Drive;
     RobotMap.drive.arcadeDrive(moveValue, turnValue);
   }
 
-  public double getRotateAngle () {
-		return rotateToAnglePidController.getSetpoint();
-  }
-  
-  public void setRotateAngle(double angle){
-
-		RobotMap.gyro.reset();
-		RobotMap.gyro.zeroYaw();
-		
-		rotateToAnglePidController.setSetpoint(angle);
-		rotateToAnglePidController.enable();
-  }
- 
-  @Override
-	public void initSendable(SendableBuilder builder) {
-		builder.addDoubleProperty("navx/angle", () -> { return RobotMap.gyro.getAngle(); }, null);
-		builder.addBooleanProperty("resetGyro", () -> { return false; }, (value) -> {
-			if (value) {
-       // rotateToAnglePidController.reset();
-        // rotateToAnglePidController.enable();
-				RobotMap.gyro.reset();
-        RobotMap.gyro.zeroYaw();
-        
-      }
-    });
-  }
-
   public double getDistance() {
 		SmartDashboard.putNumber("LeftEncoder", RobotMap.driveEncoderLeft.getDistance());
 		SmartDashboard.putNumber("RightEncoder", -RobotMap.driveEncoderRight.getDistance());
 		return ((RobotMap.driveEncoderLeft.getDistance() - RobotMap.driveEncoderRight.getDistance()) / 2.0);
   }
-  
-  @Override
-  public void setPIDSourceType(PIDSourceType pidSource) {
-
-  }
-
-	@Override
-	public PIDSourceType getPIDSourceType() {
-		// This is for the drive train encoders
-		return PIDSourceType.kDisplacement;
-	}
-
-	@Override
-	public double pidGet() {
-		// This is for the drive train encoders
-		return getDistance();
-	} 
 }
